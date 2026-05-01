87b252879f98074cdbc3527a1339d3c25b57eec0
Whi7erose <r4v1dutz3n4j17h@gmail.com>
2026-05-01 14:03:33 +0530

    Add project files.

---
+++ QwertySubstitutionCipher/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace QwertySubstitutionCipher
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txtSimpleText = new TextBox();
+            label2 = new Label();
+            txtCipherText = new TextBox();
+            btnEncrypt = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(33, 69);
+            label1.Name = "label1";
+            label1.Size = new Size(69, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Simple_Text";
+            label1.Click += label1_Click;
+            // 
+            // txtSimpleText
+            // 
+            txtSimpleText.Location = new Point(33, 87);
+            txtSimpleText.Name = "txtSimpleText";
+            txtSimpleText.Size = new Size(715, 23);
+            txtSimpleText.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(33, 176);
+            label2.Name = "label2";
+            label2.Size = new Size(68, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Cipher_Text";
+            // 
+            // txtCipherText
+            // 
+            txtCipherText.Location = new Point(33, 194);
+            txtCipherText.Name = "txtCipherText";
+            txtCipherText.Size = new Size(715, 23);
+            txtCipherText.TabIndex = 3;
+            // 
+            // btnEncrypt
+            // 
+            btnEncrypt.Location = new Point(33, 315);
+            btnEncrypt.Name = "btnEncrypt";
+            btnEncrypt.Size = new Size(75, 23);
+            btnEncrypt.TabIndex = 4;
+            btnEncrypt.Text = "Encrypt";
+            btnEncrypt.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(114, 315);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 5;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(207, 315);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 6;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(310, 315);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 7;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(btnEncrypt);
+            Controls.Add(txtCipherText);
+            Controls.Add(label2);
+            Controls.Add(txtSimpleText);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtSimpleText;
+        private Label label2;
+        private TextBox txtCipherText;
+        private Button btnEncrypt;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+    }
+}
