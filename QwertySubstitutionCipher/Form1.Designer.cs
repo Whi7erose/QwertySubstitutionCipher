f89571f97cd4a029ec3514d08fa989de88fe5abc
Whi7erose <r4v1dutz3n4j17h@gmail.com>
2026-05-01 15:44:29 +0530

    UI chnages

--- QwertySubstitutionCipher/Form1.Designer.cs
+++ QwertySubstitutionCipher/Form1.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class Form1
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,12 +13,6 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             label1 = new Label();
@@ -33,108 +20,181 @@
             label2 = new Label();
             txtCipherText = new TextBox();
             btnEncrypt = new Button();
-            button2 = new Button();
-            button3 = new Button();
-            button4 = new Button();
+            btnDecrypt = new Button();
+            btnClear = new Button();
+            btnClose = new Button();
+            titleLabel = new Label();
+            subtitleLabel = new Label();
             SuspendLayout();
+
+            // 
+            // Form Background
+            // 
+            this.BackColor = Color.FromArgb(10, 10, 10);
+
+            // 
+            // titleLabel
+            // 
+            titleLabel.AutoSize = true;
+            titleLabel.Font = new Font("Courier New", 20F, FontStyle.Bold);
+            titleLabel.ForeColor = Color.FromArgb(0, 255, 70);
+            titleLabel.Location = new Point(30, 20);
+            titleLabel.Name = "titleLabel";
+            titleLabel.Text = "> QWERTY CIPHER //";
+
+            // 
+            // subtitleLabel
+            // 
+            subtitleLabel.AutoSize = true;
+            subtitleLabel.Font = new Font("Courier New", 8F);
+            subtitleLabel.ForeColor = Color.FromArgb(0, 180, 50);
+            subtitleLabel.Location = new Point(33, 58);
+            subtitleLabel.Name = "subtitleLabel";
+            subtitleLabel.Text = "[ SUBSTITUTION ENCRYPTION TOOL v1.0 ]";
+
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(33, 69);
+            label1.Font = new Font("Courier New", 9F, FontStyle.Bold);
+            label1.ForeColor = Color.FromArgb(0, 255, 70);
+            label1.Location = new Point(33, 90);
             label1.Name = "label1";
-            label1.Size = new Size(69, 15);
-            label1.TabIndex = 0;
-            label1.Text = "Simple_Text";
-            label1.Click += label1_Click;
+            label1.Text = "// PLAIN TEXT INPUT:";
+
             // 
             // txtSimpleText
             // 
-            txtSimpleText.Location = new Point(33, 87);
+            txtSimpleText.BackColor = Color.FromArgb(15, 25, 15);
+            txtSimpleText.ForeColor = Color.FromArgb(0, 255, 70);
+            txtSimpleText.Font = new Font("Courier New", 10F);
+            txtSimpleText.BorderStyle = BorderStyle.FixedSingle;
+            txtSimpleText.Location = new Point(33, 112);
+            txtSimpleText.Multiline = true;
             txtSimpleText.Name = "txtSimpleText";
-            txtSimpleText.Size = new Size(715, 23);
+            txtSimpleText.ScrollBars = ScrollBars.Vertical;
+            txtSimpleText.Size = new Size(730, 100);
             txtSimpleText.TabIndex = 1;
+
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(33, 176);
+            label2.Font = new Font("Courier New", 9F, FontStyle.Bold);
+            label2.ForeColor = Color.FromArgb(0, 255, 70);
+            label2.Location = new Point(33, 228);
             label2.Name = "label2";
-            label2.Size = new Size(68, 15);
-            label2.TabIndex = 2;
-            label2.Text = "Cipher_Text";
+            label2.Text = "// CIPHER TEXT OUTPUT:";
+
             // 
             // txtCipherText
             // 
-            txtCipherText.Location = new Point(33, 194);
+            txtCipherText.BackColor = Color.FromArgb(15, 25, 15);
+            txtCipherText.ForeColor = Color.FromArgb(0, 255, 70);
+            txtCipherText.Font = new Font("Courier New", 10F);
+            txtCipherText.BorderStyle = BorderStyle.FixedSingle;
+            txtCipherText.Location = new Point(33, 250);
+            txtCipherText.Multiline = true;
             txtCipherText.Name = "txtCipherText";
-            txtCipherText.Size = new Size(715, 23);
+            txtCipherText.ScrollBars = ScrollBars.Vertical;
+            txtCipherText.Size = new Size(730, 100);
             txtCipherText.TabIndex = 3;
+
             // 
             // btnEncrypt
             // 
-            btnEncrypt.Location = new Point(33, 315);
+            btnEncrypt.BackColor = Color.FromArgb(0, 100, 30);
+            btnEncrypt.ForeColor = Color.FromArgb(0, 255, 70);
+            btnEncrypt.FlatStyle = FlatStyle.Flat;
+            btnEncrypt.FlatAppearance.BorderColor = Color.FromArgb(0, 255, 70);
+            btnEncrypt.Font = new Font("Courier New", 9F, FontStyle.Bold);
+            btnEncrypt.Location = new Point(33, 375);
             btnEncrypt.Name = "btnEncrypt";
-            btnEncrypt.Size = new Size(75, 23);
+            btnEncrypt.Size = new Size(110, 35);
             btnEncrypt.TabIndex = 4;
-            btnEncrypt.Text = "Encrypt";
-            btnEncrypt.UseVisualStyleBackColor = true;
+            btnEncrypt.Text = "[ ENCRYPT ]";
+            btnEncrypt.UseVisualStyleBackColor = false;
+            btnEncrypt.Click += btnEncrypt_Click;
+
             // 
-            // button2
+            // btnDecrypt
             // 
-            button2.Location = new Point(114, 315);
-            button2.Name = "button2";
-            button2.Size = new Size(75, 23);
-            button2.TabIndex = 5;
-            button2.Text = "button2";
-            button2.UseVisualStyleBackColor = true;
+            btnDecrypt.BackColor = Color.FromArgb(0, 60, 20);
+            btnDecrypt.ForeColor = Color.FromArgb(0, 255, 70);
+            btnDecrypt.FlatStyle = FlatStyle.Flat;
+            btnDecrypt.FlatAppearance.BorderColor = Color.FromArgb(0, 255, 70);
+            btnDecrypt.Font = new Font("Courier New", 9F, FontStyle.Bold);
+            btnDecrypt.Location = new Point(155, 375);
+            btnDecrypt.Name = "btnDecrypt";
+            btnDecrypt.Size = new Size(110, 35);
+            btnDecrypt.TabIndex = 5;
+            btnDecrypt.Text = "[ DECRYPT ]";
+            btnDecrypt.UseVisualStyleBackColor = false;
+            btnDecrypt.Click += btnDecrypt_Click;
+
             // 
-            // button3
+            // btnClear
             // 
-            button3.Location = new Point(207, 315);
-            button3.Name = "button3";
-            button3.Size = new Size(75, 23);
-            button3.TabIndex = 6;
-            button3.Text = "button3";
-            button3.UseVisualStyleBackColor = true;
+            btnClear.BackColor = Color.FromArgb(40, 30, 0);
+            btnClear.ForeColor = Color.FromArgb(255, 200, 0);
+            btnClear.FlatStyle = FlatStyle.Flat;
+            btnClear.FlatAppearance.BorderColor = Color.FromArgb(255, 200, 0);
+            btnClear.Font = new Font("Courier New", 9F, FontStyle.Bold);
+            btnClear.Location = new Point(277, 375);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(110, 35);
+            btnClear.TabIndex = 6;
+            btnClear.Text = "[ CLEAR ]";
+            btnClear.UseVisualStyleBackColor = false;
+            btnClear.Click += btnClear_Click;
+
             // 
-            // button4
+            // btnClose
             // 
-            button4.Location = new Point(310, 315);
-            button4.Name = "button4";
-            button4.Size = new Size(75, 23);
-            button4.TabIndex = 7;
-            button4.Text = "button4";
-            button4.UseVisualStyleBackColor = true;
+            btnClose.BackColor = Color.FromArgb(60, 10, 10);
+            btnClose.ForeColor = Color.FromArgb(255, 60, 60);
+            btnClose.FlatStyle = FlatStyle.Flat;
+            btnClose.FlatAppearance.BorderColor = Color.FromArgb(255, 60, 60);
+            btnClose.Font = new Font("Courier New", 9F, FontStyle.Bold);
+            btnClose.Location = new Point(399, 375);
+            btnClose.Name = "btnClose";
+            btnClose.Size = new Size(110, 35);
+            btnClose.TabIndex = 7;
+            btnClose.Text = "[ EXIT ]";
+            btnClose.UseVisualStyleBackColor = false;
+            btnClose.Click += btnClose_Click;
+
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(button4);
-            Controls.Add(button3);
-            Controls.Add(button2);
-            Controls.Add(btnEncrypt);
-            Controls.Add(txtCipherText);
-            Controls.Add(label2);
-            Controls.Add(txtSimpleText);
+            Controls.Add(titleLabel);
+            Controls.Add(subtitleLabel);
             Controls.Add(label1);
+            Controls.Add(txtSimpleText);
+            Controls.Add(label2);
+            Controls.Add(txtCipherText);
+            Controls.Add(btnEncrypt);
+            Controls.Add(btnDecrypt);
+            Controls.Add(btnClear);
+            Controls.Add(btnClose);
             Name = "Form1";
-            Text = "Form1";
+            Text = "QwertySubstitutionCipher";
             ResumeLayout(false);
             PerformLayout();
         }
 
-        #endregion
-
         private Label label1;
-        private TextBox txtSimpleText;
         private Label label2;
+        private Label titleLabel;
+        private Label subtitleLabel;
+        private TextBox txtSimpleText;
         private TextBox txtCipherText;
         private Button btnEncrypt;
-        private Button button2;
-        private Button button3;
-        private Button button4;
+        private Button btnDecrypt;
+        private Button btnClear;
+        private Button btnClose;
     }
 }
--- QwertySubstitutionCipher/Form1.cs
+++ QwertySubstitutionCipher/Form1.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Windows.Forms;
+
 namespace QwertySubstitutionCipher
 {
     public partial class Form1 : Form
@@ -7,9 +10,173 @@ namespace QwertySubstitutionCipher
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void btnClose_Click(object sender, EventArgs e)
         {
+            Application.Exit();
+        }
 
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtSimpleText.Clear();
+            txtCipherText.Clear();
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(txtSimpleText.Text))
+                {
+                    string b = txtSimpleText.Text;
+                    string secondtxt = string.Empty;
+                    foreach (char txt in b)
+                    {
+                        char c;
+                        if (txt == 'A') { c = 'Q'; }
+                        else if (txt == 'a') { c = 'q'; }
+                        else if (txt == 'B') { c = 'W'; }
+                        else if (txt == 'b') { c = 'w'; }
+                        else if (txt == 'C') { c = 'E'; }
+                        else if (txt == 'c') { c = 'e'; }
+                        else if (txt == 'D') { c = 'R'; }
+                        else if (txt == 'd') { c = 'r'; }
+                        else if (txt == 'E') { c = 'T'; }
+                        else if (txt == 'e') { c = 't'; }
+                        else if (txt == 'F') { c = 'Y'; }
+                        else if (txt == 'f') { c = 'y'; }
+                        else if (txt == 'G') { c = 'U'; }
+                        else if (txt == 'g') { c = 'u'; }
+                        else if (txt == 'H') { c = 'I'; }
+                        else if (txt == 'h') { c = 'i'; }
+                        else if (txt == 'I') { c = 'O'; }
+                        else if (txt == 'i') { c = 'o'; }
+                        else if (txt == 'J') { c = 'P'; }
+                        else if (txt == 'j') { c = 'p'; }
+                        else if (txt == 'K') { c = 'A'; }
+                        else if (txt == 'k') { c = 'a'; }
+                        else if (txt == 'L') { c = 'S'; }
+                        else if (txt == 'l') { c = 's'; }
+                        else if (txt == 'M') { c = 'D'; }
+                        else if (txt == 'm') { c = 'd'; }
+                        else if (txt == 'N') { c = 'F'; }
+                        else if (txt == 'n') { c = 'f'; }
+                        else if (txt == 'O') { c = 'G'; }
+                        else if (txt == 'o') { c = 'g'; }
+                        else if (txt == 'P') { c = 'H'; }
+                        else if (txt == 'p') { c = 'h'; }
+                        else if (txt == 'Q') { c = 'J'; }
+                        else if (txt == 'q') { c = 'j'; }
+                        else if (txt == 'R') { c = 'K'; }
+                        else if (txt == 'r') { c = 'k'; }
+                        else if (txt == 'S') { c = 'L'; }
+                        else if (txt == 's') { c = 'l'; }
+                        else if (txt == 'T') { c = 'Z'; }
+                        else if (txt == 't') { c = 'z'; }
+                        else if (txt == 'U') { c = 'X'; }
+                        else if (txt == 'u') { c = 'x'; }
+                        else if (txt == 'V') { c = 'C'; }
+                        else if (txt == 'v') { c = 'c'; }
+                        else if (txt == 'W') { c = 'V'; }
+                        else if (txt == 'w') { c = 'v'; }
+                        else if (txt == 'X') { c = 'B'; }
+                        else if (txt == 'x') { c = 'b'; }
+                        else if (txt == 'Y') { c = 'N'; }
+                        else if (txt == 'y') { c = 'n'; }
+                        else if (txt == 'Z') { c = 'M'; }
+                        else if (txt == 'z') { c = 'm'; }
+                        else { c = txt; }
+                        secondtxt += c.ToString();
+                    }
+                    txtCipherText.Text = secondtxt;
+                }
+                else
+                {
+                    MessageBox.Show("Please Enter the Simple Text to Encrypt!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(txtCipherText.Text))
+                {
+                    string b = txtCipherText.Text;
+                    string secondtxt = string.Empty;
+                    foreach (char txt in b)
+                    {
+                        char c;
+                        if (txt == 'Q') { c = 'A'; }
+                        else if (txt == 'q') { c = 'a'; }
+                        else if (txt == 'W') { c = 'B'; }
+                        else if (txt == 'w') { c = 'b'; }
+                        else if (txt == 'E') { c = 'C'; }
+                        else if (txt == 'e') { c = 'c'; }
+                        else if (txt == 'R') { c = 'D'; }
+                        else if (txt == 'r') { c = 'd'; }
+                        else if (txt == 'T') { c = 'E'; }
+                        else if (txt == 't') { c = 'e'; }
+                        else if (txt == 'Y') { c = 'F'; }
+                        else if (txt == 'y') { c = 'f'; }
+                        else if (txt == 'U') { c = 'G'; }
+                        else if (txt == 'u') { c = 'g'; }
+                        else if (txt == 'I') { c = 'H'; }
+                        else if (txt == 'i') { c = 'h'; }
+                        else if (txt == 'O') { c = 'I'; }
+                        else if (txt == 'o') { c = 'i'; }
+                        else if (txt == 'P') { c = 'J'; }
+                        else if (txt == 'p') { c = 'j'; }
+                        else if (txt == 'A') { c = 'K'; }
+                        else if (txt == 'a') { c = 'k'; }
+                        else if (txt == 'S') { c = 'L'; }
+                        else if (txt == 's') { c = 'l'; }
+                        else if (txt == 'D') { c = 'M'; }
+                        else if (txt == 'd') { c = 'm'; }
+                        else if (txt == 'F') { c = 'N'; }
+                        else if (txt == 'f') { c = 'n'; }
+                        else if (txt == 'G') { c = 'O'; }
+                        else if (txt == 'g') { c = 'o'; }
+                        else if (txt == 'H') { c = 'P'; }
+                        else if (txt == 'h') { c = 'p'; }
+                        else if (txt == 'J') { c = 'Q'; }
+                        else if (txt == 'j') { c = 'q'; }
+                        else if (txt == 'K') { c = 'R'; }
+                        else if (txt == 'k') { c = 'r'; }
+                        else if (txt == 'L') { c = 'S'; }
+                        else if (txt == 'l') { c = 's'; }
+                        else if (txt == 'Z') { c = 'T'; }
+                        else if (txt == 'z') { c = 't'; }
+                        else if (txt == 'X') { c = 'U'; }
+                        else if (txt == 'x') { c = 'u'; }
+                        else if (txt == 'C') { c = 'V'; }
+                        else if (txt == 'c') { c = 'v'; }
+                        else if (txt == 'V') { c = 'W'; }
+                        else if (txt == 'v') { c = 'w'; }
+                        else if (txt == 'B') { c = 'X'; }
+                        else if (txt == 'b') { c = 'x'; }
+                        else if (txt == 'N') { c = 'Y'; }
+                        else if (txt == 'n') { c = 'y'; }
+                        else if (txt == 'M') { c = 'Z'; }
+                        else if (txt == 'm') { c = 'z'; }
+                        else { c = txt; }
+                        secondtxt += c.ToString();
+                    }
+                    txtSimpleText.Text = secondtxt;
+                }
+                else
+                {
+                    MessageBox.Show("Please Enter the Cipher Text to Decrypt!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
